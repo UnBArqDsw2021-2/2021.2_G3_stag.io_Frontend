6a6d122de24cdeea0295d3cce6b7d5804500af7e
joaovaladao <joaovaladao161274@gmail.com>
2022-04-04 14:43:51 -0300

    Definindo estrutura do projeto e adição da Navbar
    Co-authored-by: gabrielavelino <blackzinh8@gmail.com>

--- stag.io/src/pages/Inicio/Inicio.jsx
+++ stag.io/src/pages/Inicio/Inicio.jsx
@@ -3,7 +3,22 @@ import './Inicio.css';
 function Inicio() {
   return (
     <div className="Inicio">
-      <h1>Inicio</h1>
+      
+      <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#">
+            <img className="img-fluid" 
+              src={`${process.env.PUBLIC_URL}/logo/logo-white.png`} width = "50" alt="logo"/>
+            </a>
+            <div class="collapse navbar-collapse">
+                <ul>
+                  <h2 class="title">Stag.io</h2>
+                </ul>
+            </div>
+            <form class="d-flex">
+              <button class="btn btn-dark buttonLoggin" type="submit">Login</button>
+            </form>
+        </nav>
+
     </div>
   );
 }
