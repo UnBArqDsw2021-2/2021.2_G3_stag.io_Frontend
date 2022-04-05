dce62c29f41a442a655f0c7b47c454d159529147
joaovaladao <joaovaladao161274@gmail.com>
2022-04-05 15:26:55 -0300

    Definição da parte estética da página de início
    Co-authored-by: gabrielavelino <blackzinh8@gmail.com>

--- stag.io/src/pages/Inicio/Inicio.jsx
+++ stag.io/src/pages/Inicio/Inicio.jsx
@@ -1,9 +1,11 @@
 import './Inicio.css';
+import Mulher from './assets/mulher_inicio.png'
 
 function Inicio() {
   return (
     <div className="Inicio">
       
+      {/* Barra de Navegação */}
       <nav class="navbar navbar-expand-lg navbar-light">
             <a class="navbar-brand" href="#">
             <img className="img-fluid" 
@@ -18,6 +20,33 @@ function Inicio() {
               <button class="btn btn-dark buttonLoggin" type="submit">Login</button>
             </form>
         </nav>
+
+
+        {/* Container que aloca a página */}
+        <div class="container-fluid margin">
+            <div class="container margin">
+                <div class="row">
+                    <div class="col-lg-8 marginTop">
+                      <div class="container">
+                        <h3 class="subTitle">
+                            Qual a sua Busca?
+                        </h3>
+                        <div class="row">
+                          <div class="col-xs-6 col-sm-6 col-md-6">
+                            <a href="" class="btn btn-lg btn-outline-dark btn-block colorButton">Estagiar</a>
+                          </div>
+                          <div class="col-xs-6 col-sm-6 col-md-6">
+                            <a href="" class="btn btn-lg btn-outline-dark btn-block colorButton">Estagiários</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  <div class="col-lg-4">
+                    <img src={Mulher} class="imagem"/>
+                  </div>
+                </div>
+            </div>
+        </div>
 
     </div>
   );
