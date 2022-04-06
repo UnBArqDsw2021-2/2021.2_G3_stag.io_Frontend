ef69647fb408f1dc42123378b0b517391c897834
joaovaladao <joaovaladao161274@gmail.com>
2022-04-06 17:15:58 -0300

    Lógica do Pop Up para loggin

--- stag.io/src/components/Login/login.jsx
+++ stag.io/src/components/Login/login.jsx
@@ -1,12 +1,24 @@
 import './login.css';
+import React from 'react';
+import Lock from './assets/lock.png'
 
-function Login() {
-    return (
-        <div className="Login">
-          
-    </div>
-
-    );
+function Popup(props){
+    return(props.trigger) ? (
+        <div className='popup'>
+            <div className="popup-inner">
+                {props.children }
+                 <section className="login-dark">
+                    <button className="close-btn " onClick={() => props.setTrigger(false)}>X</button>
+                     <form method="post">
+                         <img src={Lock} className="imagemLogin"/>
+                         <div className="mb-3"><input className="form-control" type="email" name="email" placeholder="CNPJ/CPF" /></div>
+                         <div className="mb-3"><input className="form-control" type="password" name="password" placeholder="Senha" /></div>
+                         <div className="mb-3"><button className="btn btn-secondary d-block w-100 buttonColor" type="submit">Log In</button></div><a className="forgot linkOptions" href="#">Forgot your email or password?</a>
+                     </form>
+                 </section>
+            </div>
+        </div>
+    ):"";
 }
-  
-  export default Login;
+
+export default Popup;
--- stag.io/src/pages/Inicio/Inicio.jsx
+++ stag.io/src/pages/Inicio/Inicio.jsx
@@ -1,62 +1,74 @@
 import './Inicio.css';
 import Mulher from './assets/mulher_inicio.png'
+
 import { useState } from 'react';
-import Login from '../../components/Login/login';
+import Popup from '../../components/Login/login';
 
 
 
-function Inicio() {
+function Inicio() { 
+  const [buttonPopup,setButtonPopup] = useState (false);
   
-  /*const [showLogin,setShowLogin] = useState (false);
-  const handleLogin = ()=>{setShowLogin((showLogin)=>!showLogin),console.log(showLogin)};*/
+  const handleLogin = (e)=>{
+    e.preventDefault();
+    setButtonPopup((buttonPopup)=>!buttonPopup)
+
+  };
+
+  console.log (buttonPopup);
 
   return (
     <div className="Inicio">
       
       {/* Barra de Navegação */}
-      <nav class="navbar navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="#">
+      <nav className="navbar navbar-expand-lg navbar-light">
+            <a className="navbar-brand" href="#">
             <img className="img-fluid" 
               src={`${process.env.PUBLIC_URL}/logo/logo-white.png`} width = "50" alt="logo"/>
             </a>
-            <div class="collapse navbar-collapse">
+            <div className="collapse navbar-collapse">
                 <ul>
-                  <h2 class="title">Stag.io</h2>
+                  <h2 className="title">Stag.io</h2>
                 </ul>
             </div>
-            <form class="d-flex">
-              <button class="btn btn-dark buttonLoggin" type="submit" size="sm" /*onClick={handleLogin}*/ >
-              <h2 class="titleButton">Login</h2>
+            <form className="d-flex">
+
+              <button className="btn btn-dark buttonLoggin" type="submit" size="sm" onClick={(e) => handleLogin(e)} >
+                <h2 className="titleButton">Login</h2>
               </button>
             </form>
         </nav>
 
-
         {/* Container que aloca a página */}
-        <div class="container-fluid margin">
-            <div class="container margin">
-                <div class="row">
-                    <div class="col-lg-8 marginTop">
-                      <div class="container">
-                        <h3 class="subTitle">
+        <div className="container-fluid margin">
+            <div className="container margin">
+                <div className="row">
+                    <div className="col-lg-8 marginTop">
+                      <div className="container">
+                        <h3 className="subTitle">
                             Qual a sua Busca?
                         </h3>
-                        <div class="row">
-                          <div class="col-xs-6 col-sm-6 col-md-6">
-                            <a href="" class="btn btn-lg btn-outline-dark btn-block colorButton">Estagiar</a>
+                        <div className="row">
+                          <div className="col-xs-6 col-sm-6 col-md-6">
+                            <a href="" className="btn btn-lg btn-outline-dark btn-block colorButton">Estagiar</a>
                           </div>
-                          <div class="col-xs-6 col-sm-6 col-md-6">
-                            <a href="" class="btn btn-lg btn-outline-dark btn-block colorButton">Estagiários</a>
+                          <div className="col-xs-6 col-sm-6 col-md-6">
+                            <a href="" className="btn btn-lg btn-outline-dark btn-block colorButton">Estagiários</a>
                           </div>
                         </div>
                       </div>
                     </div>
-                  <div class="col-lg-4">
-                    <img src={Mulher} class="imagem"/>
+                  <div className="col-lg-4">
+                    <img src={Mulher} className="imagem"/>
                   </div>
                 </div>
             </div>
         </div>
+
+        {/* Criação de PopUp */}
+        <Popup
+          trigger={buttonPopup} setTrigger={setButtonPopup}>
+        </Popup>
 
     </div>
   );
