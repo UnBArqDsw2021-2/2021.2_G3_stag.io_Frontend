7d74c3157f0105445a69182a364922af02a709eb
Guilherme de Morais Richter <guilhermemoraisr@gmail.com>
2022-04-11 09:39:17 -0300

    Template das páginas de Descrição e Situação da Vaga

--- stag.io/src/App.js
+++ stag.io/src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
+import DescricaoVaga from "./pages/DescricaoVaga/DescricaoVaga";
+import SituacaoVaga from "./pages/SituacaoVaga/SituacaoVaga";
 import Inicio from "./pages/Inicio/Inicio";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio />}></Route>
+        <Route path="/DescricaoVaga" element={<DescricaoVaga />}></Route>
+        <Route path="/SituacaoVaga" element={<SituacaoVaga />}></Route>
       </Routes>
     </div>
   );
