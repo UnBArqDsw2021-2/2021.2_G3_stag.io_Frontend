3831f6a992356b123815e8c01e14e841e5e12455
GRVial <guivial2010@gmail.com>
2022-04-11 22:30:09 -0300

    comeco da navbar

--- stag.io/src/pages/SituacaoVaga/SituacaoVaga.jsx
+++ stag.io/src/pages/SituacaoVaga/SituacaoVaga.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './SituacaoVaga.css';
+import Navbar from "../navBar/NavBar"
 
 function SituacaoVaga() {
     return (
         <div className="detalhes-candidato">
-
+            <Navbar></Navbar>
             {/* Container que aloca a página */}
             <h1 className="subTitle">
                 Engenheiro de Dados
